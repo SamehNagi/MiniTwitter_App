d2dc0b23abf40d13358bd78abcb9def2c7560ca6
saasbook <saasbook@saasbook.(none)>
2015-01-04 13:16:35 -0400

    My App

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe PostsController, :type => :controller do
 
+  include Devise::TestHelpers
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = FactoryGirl.create(:user)
+    sign_in user
+  end
+
+  before(:each) do
+    request.env["HTTP_REFERER"] = "where_i_came_from"
+  end
+
+  describe "POST create" do
+    it "shouldn't create a post if data is invalid" do
+      expect{post :create}.to change(Post, :count).by(0)
+    end
+  end
+
+  describe "POST create success" do
+    it "should create a post if data is valid" do
+      expect{post :create, {'post' => {'content' =>"posts controller rspec"}}}.to change(Post, :count).by(1)
+    end
+  end
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    
-  end
+    sequence(:email) { |n| "user#{n}@example.com"}
+    password "12345678"
+    password_confirmation "12345678"
 
+    factory :admin do
+      admin true
+    end    
+  end
 end
